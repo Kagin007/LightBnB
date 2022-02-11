745a9318c8f07224f9ad2f9abc0f142c166348dc
Adam Schulte <adamschulte148@hotmail.com>
2022-02-11 01:52:45 +0000

    refactor database

--- LightBnB_WebApp-master/server/database.js
+++ LightBnB_WebApp-master/server/database.js
@@ -1,14 +1,14 @@
 const properties = require('./json/properties.json');
 const users = require('./json/users.json');
-const { Pool } = require('pg');
+const db = require('../db')
+// const { Pool } = require('pg')
 
-//pool for fetching database info
-const pool = new Pool({
-  user: 'vagrant',
-  password: '123',
-  host: 'localhost',
-  database: 'lightbnb'
-});
+// const pool = new Pool({
+//   user: 'vagrant',
+//   password: '123',
+//   host: 'localhost',
+//   database: 'lightbnb'
+// })
 
 /// Users
 
@@ -18,7 +18,7 @@ const pool = new Pool({
  * @return {Promise<{}>} A promise to the user.
  */
 const getUserWithEmail = (email) => {
-  return pool
+  return db
     .query(`
       SELECT * FROM users
       WHERE email = $1`, [email]
@@ -42,7 +42,7 @@ exports.getUserWithEmail = getUserWithEmail;
  * @return {Promise<{}>} A promise to the user.
  */
 const getUserWithId = function(id) {
-  return pool
+  return db
     .query(`
       SELECT * FROM users
       WHERE id = $1`, [id])
@@ -66,7 +66,7 @@ exports.getUserWithId = getUserWithId;
  * @return {Promise<{}>} A promise to the user.
  */
 const addUser =  function(user) {
-  return pool
+  return db
     .query(
       `INSERT INTO users (name, email, password)
       VALUES ($1, $2, $3) 
@@ -90,7 +90,7 @@ exports.addUser = addUser;
  * @return {Promise<[{}]>} A promise to the reservations.
  */
 const getAllReservations = function(guest_id, limit = 10) {
-  return pool
+  return db
     .query(
       `SELECT * FROM reservations
       WHERE guest_id = $1
@@ -115,14 +115,13 @@ exports.getAllReservations = getAllReservations;
  */
 const getAllProperties = (options, limit = 10) => {
   const queryParams = [];
-  // 2
+
   let queryString = `
   SELECT properties.*, avg(property_reviews.rating) as average_rating
   FROM properties
   JOIN property_reviews ON properties.id = property_id
   `;
 
-  // 3
   if (options.city) {
     queryParams.push(`%${options.city}%`);
     queryString += queryParams.length > 1 && 'AND' || 'WHERE ';
@@ -152,7 +151,6 @@ const getAllProperties = (options, limit = 10) => {
     queryString += ` user LIKE $${queryParams.length}`;
   }
 
-  // 4
   queryParams.push(limit);
   queryString += `
   GROUP BY properties.id
@@ -160,11 +158,15 @@ const getAllProperties = (options, limit = 10) => {
   LIMIT $${queryParams.length};
   `;
 
-  // 5
   console.log(queryString, queryParams);
 
-  // 6
-  return pool.query(queryString, queryParams).then((res) => res.rows);
+  return db.query(queryString, queryParams)
+    .then((res) => {
+      return res.rows
+    })
+    .catch((err) => {
+      return err.message
+    })
 };
 
 exports.getAllProperties = getAllProperties;
@@ -186,7 +188,7 @@ const addProperty = function(property) {
     property.owner_id, property.title, property.description, property.thumbnail_photo_url, property.cover_photo_url, property.cost_per_night, property.street, property.city, property.province, property.post_code, property.country, property.parking_spaces, property.number_of_bathrooms, property.number_of_bedrooms
   ]
 
-  return pool
+  return db
     .query(SQLstatement, stringInterpolation)
     .then( res => {
       console.log('from then', res)
